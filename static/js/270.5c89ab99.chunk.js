d038bf77b7b6a3f38571796185c1d5aecdd1ed72
andsnk <127045868+andsnk@users.noreply.github.com>
2024-02-02 22:03:48 +0000

    Deploying to gh-pages from @ andsnk/car-rental@65c332a379b0807068f106f05a4473768acbae25 🚀

--- static/js/270.5c89ab99.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[270],{270:function(e,t,r){r.r(t);r(791);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:(0,a.jsx)("h2",{children:"Catalog"})})}}}]);
-//# sourceMappingURL=270.5c89ab99.chunk.js.map
